c96c5856362203102fab5380c3a697b53189182a
Pablo Puerto <pablo.puerto@crem.ch>
2019-03-01 14:37:12 +0100

    add doc and c.remove(force) in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,23 +6,23 @@ from zerobnl.config import *
 @pytest.fixture
 def clean_docker(scope="function"):
     """
-
+    This fixture stop all the docker containers.
 	"""
     import docker
 
-    yield True
+    yield 0
     client = docker.from_env()
-    for c in client.containers.list():
-        c.kill()
+    for c in client.containers.list(all=True):
+        c.remove(force=True)
 
 @pytest.fixture
 def clean_folder(scope="function"):
     """
-
+    This fixture removes the temporary folder (if it exists).
 	"""
     import shutil
 
-    yield True
+    yield 0
     try:
         shutil.rmtree(TEMP_FOLDER)
     except FileNotFoundError:
